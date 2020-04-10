76ff7b0b96038dfcefc0db8e8cb7670b01cc85fa
gh2WHY <1441829719@qq.com>
2020-04-10 17:08:45 +0800

    node模块化的理解

---
+++ day01/a.js
@@ -0,0 +1,5 @@
+console.log("a.js");
+let num = 5;
+//并没有使用exports向外面暴露数据
+exports.num = 5;
+// module.exports= {};
---
+++ day01/b.js
@@ -0,0 +1,10 @@
+let a = require("./a");
+let b = require('./a');
+let c  = require('./a');
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+
+
