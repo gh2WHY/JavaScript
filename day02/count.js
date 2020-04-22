030cd704987f1bfc200394530528b5a1b76c7782
gh2WHY <1441829719@qq.com>
2020-04-22 20:16:50 +0800

    express中间件练习

--- day02/count.js
+++
@@ -1,9 +0,0 @@
-const {EventEmitter} = require("events");
-// 定义子类
-class Emitter extends EventEmitter{};
-// 创建实例
-const myEmitter = new MyEmitter();
-myEmitter.on('event', () => {});
-myEmitter.on('event', () => {});
-console.log(EventEmitter.listenerCount(myEmitter, 'event'));
-// Prints: 2
