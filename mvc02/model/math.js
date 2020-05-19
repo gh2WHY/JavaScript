13ba4a2a0abedbd1c3672f37980a8cb22ee0ab3f
gh2WHY <1441829719@qq.com>
2020-05-19 20:22:54 +0800

    bfc 复习

--- mvc02/model/math.js
+++
@@ -1,9 +0,0 @@
-exports.cacul = function (num) {
-    let resultArr = [];
-    for(let i = 0; i <= num ; i++) {
-        if(num % i == 0) {
-            resultArr.push(i);
-        }
-    }
-    return resultArr;
-}
